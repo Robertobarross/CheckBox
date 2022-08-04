1148e09c5163c993f69df942beac9daedaf7a6e4
Roberto Barrosss <robertobarros27esp@gmail.com>
2022-08-03 22:05:02 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Check from './src/CheckBox/checkBox';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Check />
       <StatusBar style="auto" />
     </View>
   );
